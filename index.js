17f821b18f251706a46dfa88d5e26425c0c48395
Travis You <1244098000@qq.com>
2022-06-07 13:33:04 +0800

    docs: update the index

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+console.log(name.toUpperCase());
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+words = sentence.split(" ")
+for (index in words) {
+    words[index] = words[index].slice(0, 1).toUpperCase() + words[index].slice(1);
+}
+console.log(words.join(" "));
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+var ans = ''
+for (index in money) {
+    if (!isNaN(money[index])) ans += money[index];
+}
+console.log(ans);
